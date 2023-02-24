b9570f2afe54c8936806e389904819b67c861bc3
Miyxzu <ezio.hasibuan@gmail.com>
2023-02-25 02:30:43 +0700

    Fixed some errors and updated code

--- C++/ifOperators.cpp
+++ C++/ifOperators.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 int main(int argc, const char** argv) {
-    cout << "Enter a number: "
+    cout << "Enter a number: ";
     int num;
-    cin >> num
+    cin >> num;
 
     string nonMulti = "Not a multiple of two\n\n";
     string multiOf = "Multiple of 2\n\n";
 
-    num % 2 ? multiOf << endl << endl : nonMulti << endl << endl;
+    num % 2 ? cout << multiOf : cout << nonMulti;
 
     cout << "Enter the temperature: ";
     int temp;
     cin >> temp;
 
-    string cold = "That temperature is cold as shit"
-    string hot = "That temperature is hot as hell"
-    string middle = "The temperature is good"
+    string cold = "That temperature is cold as shit";
+    string hot = "That temperature is hot as hell";
+    string middle = "The temperature is good";
 
 
-    temp >= 2 || temp <= 28 ? middle : 
-    temp < 2  ? cold : hot;
+    temp >= 2 || temp <= 28 ? cout << middle : 
+    temp < 2  ? cout << cold : cout << hot;
 }
