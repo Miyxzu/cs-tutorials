5503bd54ffed7f929d0b77a3344804bb432b052e
Miyxzu <ezio.hasibuan@gmail.com>
2023-05-05 09:04:03 +0700

    Remake Java program in C#

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -5,12 +5,7 @@ namespace CSharp
     {
         public static void Main(string[] args)
         {
-            String x = "This is fun!";
-
-            for(int i = 0; i < x.Length; i++)
-            {
-                Console.Write(x[i] + " ");
-            }
+            
         }
     }
 }
