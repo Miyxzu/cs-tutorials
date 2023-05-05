bdd82293156cf9800f526461d053876cb4db99ef
Miyxzu <ezio.hasibuan@gmail.com>
2023-05-05 09:04:47 +0700

    Miscellaneous files

--- Java/src/ImportBased/filingRead.java
+++ Java/src/ImportBased/filingRead.java
@@ -9,29 +9,28 @@ import java.io.IOException;
 public class filingRead {
     public static void main(String[] args) {
         try {
-            Scanner in = new Scanner(System.in);
+            try (Scanner in = new Scanner(System.in)) {
+                System.out.print("Enter the path of the file you want to read from: ");
+                String path = in.nextLine();
 
-            System.out.print("Enter the path of the file you want to read from: ");
-            String path = in.nextLine();
-
-            System.out.println();
-
-            File file = new File(path);
-            FileReader read = new FileReader(file);
-            int i;
-            if (file.exists()) {
-                System.out.println("File does exist");
-                System.out.println("Notice: Reading from file " + file.getName());
                 System.out.println();
-                System.out.println("File contents: ");
-                while ((i = read.read()) != -1) {
-                    System.out.print((char) i);
-                }
-            } else {
-                System.out.println("File does not exist");
-            }
-            read.close();
 
+                File file = new File(path);
+                FileReader read = new FileReader(file);
+                int i;
+                if (file.exists()) {
+                    System.out.println("File does exist");
+                    System.out.println("Notice: Reading from file " + file.getName());
+                    System.out.println();
+                    System.out.println("File contents: ");
+                    while ((i = read.read()) != -1) {
+                        System.out.print((char) i);
+                    }
+                } else {
+                    System.out.println("File does not exist");
+                }
+                read.close();
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
