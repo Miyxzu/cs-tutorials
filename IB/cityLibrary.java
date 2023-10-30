7908883a3960ac4158e4242770670353dd706ed4
Exsu <ezio.hasibuan@gmail.com>
2023-10-30 16:49:49 +0000

    i finished it and by finished i mean openaied it

---
+++ IB/cityLibrary.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class cityLibrary {
+    private ArrayList<book> arr = new ArrayList<>();
+    private Random rand = new Random();
+
+    public void addBook() {
+        
+    }
+
+    
+}
