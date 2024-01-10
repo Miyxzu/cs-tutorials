752caaff8c96a8964945c75fec06b2361cb58a8d
Exsu <ezio.hasibuan@gmail.com>
2024-01-10 07:40:02 +0000

    finish on replit ig

--- IB/checking.java
+++ IB/checking.java
@@ -2,7 +2,10 @@ package others;
 
 public class checking {
     public static void main(String[] args) {
-        
+        double scores[][] = new double[20][8];
+        double scoreNon2D[] = new double[10];
+        String names[] = new String[20];
+        String nameNon2D[] = new String[10];
     }
 
     int avgScore(int scores[]) {
