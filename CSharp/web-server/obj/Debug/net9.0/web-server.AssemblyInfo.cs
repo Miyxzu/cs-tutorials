b18c689c2b1b5783fa26140f55455c36a8a5e907
Exsu <ezio.hasibuan@gmail.com>
2025-10-17 13:50:12 +0100

    making one more update to the gitignore

--- CSharp/web-server/obj/Debug/net9.0/web-server.AssemblyInfo.cs
+++ CSharp/web-server/obj/Debug/net9.0/web-server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b1f146787233d8efa038083661e6bca82da6a3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602a6a2c99557ad642d98a39b4eb3975cd7f581c")]
 [assembly: System.Reflection.AssemblyProductAttribute("web-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
